f428f0e0f50db63a94f2fd1d7ef4afdd334a71e3
Cem Eteke <ceteke13@ku.edu.tr>
2019-07-17 14:23:27 +0300

    Plot update

--- plot_return.py
+++ plot_return.py
@@ -14,18 +14,19 @@ class RenamingUnpickler(pickle.Unpickler, object):
             module = 'lfd_improve.goal_model'
         return super(RenamingUnpickler, self).find_class(module, name)
 
-demo = "/Volumes/Feyyaz/MSc/lfd_improve_demos/close/"
-ex_dir = os.path.join(demo, 'ex1')
+demo = "/home/ceteke/Desktop/lfd_improve_demos/draw/"
+ex_dir = os.path.join(demo, 'ex3')
 
-values = np.loadtxt(os.path.join(ex_dir, 'values.csv'))
-values[np.argmax(values)] /= 10
+values = np.loadtxt(os.path.join(demo, 'values.csv'))
+values[4] *= 10
+#values[np.argmax(values)] /= 10
 
-with open(os.path.join(ex_dir, 'goal_model.pk'), 'rb') as fp:
+with open(os.path.join(demo, 'goal_model.pk'), 'rb') as fp:
     goal_model = RenamingUnpickler(fp).load()
 
 s2d = QS2D(goal_model, values=values)
 
-pc_data = pickle.load(open(os.path.join(demo, '1', 'pcae.pk'), 'rb'))[1:]
+pc_data = pickle.load(open(os.path.join(demo, 'ex3/greedy_4', 'pcae.pk'), 'rb'))[1:]
 pc_ts = np.array([p[0] for p in pc_data])
 pc_feats = np.array([p[1] for p in pc_data])
 pc_ts -= pc_ts[0]
@@ -42,26 +43,55 @@ per_feats = np.concatenate([d.per_feats for d in data.demos])
 pca.fit(per_feats)
 pc_feats = pca.transform(pc_feats)
 
-plt.scatter(pc_feats[:, 0], pc_feats[:, 1])
+#plt.scatter(pc_feats[:, 0], pc_feats[:, 1])
+#plt.show()
+
+returns = s2d.get_expected_return(pc_feats, arr=True)
+all_returns = np.zeros((len(pc_feats), len(pc_feats))) - 1
+
+# for t in range(len(pc_feats)):
+#     pc_seq = pc_feats[:t+1]
+#
+#     rew_arr = s2d.get_expected_return(pc_seq, arr=True)
+#     all_returns[t, :t + 1] = rew_arr
+#     returns.append(np.sum(rew_arr))
+
+im = plt.imshow(all_returns)
+plt.colorbar(im)
 plt.show()
 
-returns = []
-for t in range(len(pc_feats)):
-    pc_seq = pc_feats[:t+1]
-    returns.append(s2d.get_expected_return(pc_seq))
+pc1 = pc_feats[:54]
+pc2 = pc_feats[:55]
+
+pr1 = goal_model.hmm.predict_proba(pc1).T
+pr2 = goal_model.hmm.predict_proba(pc2)[:-1].T
+
+rew_arr1 = s2d.get_expected_return(pc1, arr=True)
+rew_arr2 = s2d.get_expected_return(pc2, arr=True)
+
+f1, ax = plt.subplots(2)
+
+im = ax[0].imshow(pr1)
+ax[1].imshow(pr2)
+
+f1.subplots_adjust(right=0.8)
+cbar_ax = f1.add_axes([0.85, 0.15, 0.05, 0.7])
+f1.colorbar(im, cax=cbar_ax)
+
+plt.show()
 
 plt.xticks(range(0, int(max(pc_ts))+1))
 plt.plot(pc_ts, returns)
-plt.title("Close")
-plt.xlabel('Time (s)')
-plt.ylabel("Return")
-plt.savefig('/Users/cem/Desktop/close_return.png', bbox_inches="tight", dpi=400)
+# plt.title("Close")
+# plt.xlabel('Time (s)')
+# plt.ylabel("Return")
+#plt.savefig('/Users/cem/Desktop/close_return.png', bbox_inches="tight", dpi=400)
 plt.show()
-
-ts = [0, 4, 5.25, 6, 7, pc_ts[-1]]
-
-for t in ts:
-    print np.argmin(np.abs(pc_ts-t))
+#
+# ts = [0, 4, 5.25, 6, 7, pc_ts[-1]]
+#
+# for t in ts:
+#     print np.argmin(np.abs(pc_ts-t))
 
 #print("Load a ply point cloud, print it, and render it")
 #pcd = o3d.io.read_point_cloud("/Volumes/Feyyaz/MSc/lfd_improve_demos/open2/1/pf_0_seg.pcd")
