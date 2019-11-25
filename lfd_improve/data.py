8535ff6dec92090b338961fb371d04e7d5cfc3a7
Cem Eteke <cem@Cems-MacBook-Pro-2.local>
2019-11-25 10:15:47 +0300

    Data dix

--- lfd_improve/data.py
+++ lfd_improve/data.py
@@ -6,28 +6,17 @@ import matplotlib.pyplot as plt
 class Demonstration(object):
     def __init__(self, demo_dir):
         self.demo_dir = demo_dir
-        robot_dir = os.path.join(self.demo_dir, 'ee_poses_wrt_obj.csv')
-        per_dir = os.path.join(self.demo_dir, 'perception.csv')
-        torque_dir = os.path.join(self.demo_dir, 'joint_torques.csv')
+        robot_dir = os.path.join(self.demo_dir, 'robot_states.pk')
+        per_dir = os.path.join(self.demo_dir, 'pcae.pk')
+        robot_data = pickle.load(open(robot_dir, 'rb'))[1:]
+        per_data = pickle.load(open(per_dir, 'rb'))[1:]
 
-        robot_data = np.loadtxt(robot_dir, delimiter=',')
-        per_data = np.loadtxt(per_dir, delimiter=',')
-        torque_data = np.loadtxt(torque_dir, delimiter=',')
+        self.times = np.array([r[0] for r in robot_data])
+        self.ee_poses = np.array([r[-1] for r in robot_data])
+        self.per_feats = np.array([p[-1] for p in per_data])
+        self.times = self.times - self.times[0]
 
-        n_points_torque = 150
-
-        N = len(torque_data)
-        idxs = np.arange(0, N, N // n_points_torque)
-        torque_data = torque_data[idxs]
-
-        #print("N tq: ", len(torque_data))
-
-        times = robot_data[:, 0]
-        self.ee_poses = robot_data[:, 1:]
-        self.per_feats = per_data[:, 1:]
-        self.torques = torque_data[:, 1:8] # Last 2 is gripper torques
-
-        self.spliner = Spliner(times, self.ee_poses)
+        self.spliner = Spliner(self.times, self.ee_poses)
         self.t, self.x, self.dx, self.ddx, self.dddx = self.spliner.get_motion
 
         # plt.plot(self.t, self.x)
